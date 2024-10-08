5cec8bd2e56ab53879b06031a3c419120dadd876
Kangho <kkh4323@naver.com>
2024-10-08 18:17:13 +0900

    add html templates for sending question

--- src/question/question.service.ts
+++ src/question/question.service.ts
@@ -8,6 +8,8 @@ import { BufferedFile } from '../minio-client/file.model';
 import { MinioClientService } from '../minio-client/minio-client.service';
 import { EmailService } from '../email/email.service';
 import { ConfigService } from '@nestjs/config';
+import * as fs from 'fs';
+import * as handlebars from 'handlebars';
 
 @Injectable()
 export class QuestionService {
@@ -36,12 +38,31 @@ export class QuestionService {
       user,
     });
     await this.questionRepository.save(newQuestion);
-    // 일반 유저에게 보내는 메일
+    // 이메일 템플릿 파일 경로
+    const templateFilePath = `${process.cwd()}/src/common/templates/question.html`;
+
+    // 템플릿 파일 읽기
+    const templateSource = fs.readFileSync(templateFilePath, 'utf8');
+
+    // 템플릿 엔진 컴파일
+    const template = handlebars.compile(templateSource);
+
+    // 템플릿에 데이터 삽입
+    const htmlToSend = template({
+      group: createQuestionDto.group,
+      eventName: createQuestionDto.eventName,
+      eventDate: createQuestionDto.eventDate,
+      participants: createQuestionDto.participants,
+      location: createQuestionDto.location,
+      zone: createQuestionDto.zone.join(', '),
+      documentAddress: newDocument,
+      personalInfo: createQuestionDto.personalInfo ? '동의함' : '동의하지 않음',
+    });
     await this.emailService.sendMail({
       // from: 'kdp@elice.io',
       to: user.email,
       subject: `[외부 대관] 엘리스랩 ${createQuestionDto.group} ${createQuestionDto.eventName} 대관 신청`,
-      html: `<h1>${createQuestionDto.group} ${createQuestionDto.eventName} 대관 신청</h1>`,
+      html: htmlToSend,
     });
     return newQuestion;
   }
